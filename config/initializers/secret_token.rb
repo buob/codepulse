bdd6e271661f298165cf4e23cb6ba16ac9d82301
Jake Buob <jake@figmints.com>
2013-09-18 23:35:46 -0400

    Run `rails new codepulse`

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Codepulse::Application.config.secret_key_base = '8dc9708c57aca1fab30290c0955d810b628ff0b6b53aab01364b336f3d2bb152ace21106da37c172d3344b692e66bdae24f8b3c8347cbe3f1523f1a387e27444'
