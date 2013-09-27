bcfe07e8c27394d251d3bd61687a48cf474da076
Jake Buob <jake@mojotech.com>
2013-09-26 22:25:31 -0400

    Create pulse after_create of user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  has_one :pulse, dependent: :destroy
+  after_create :create_pulse
 end
