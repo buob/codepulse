c8de1bae6a704453356bb6979acb0ea166311efd
Jake Buob <jake@mojotech.com>
2013-10-10 22:19:47 -0400

    Move omniauth callbacks controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Codepulse::Application.routes.draw do
   resources :pulses
 
   get "marketing/index"
-  devise_for :users
+  devise_for :users, controllers: { omniauth_callbacks: "omniauth_callbacks" }
 
   authenticated :user do
     root to: 'pulses#index', as: :authenticated_root
