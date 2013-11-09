9ee55e2d573c833fcc4026f78817564627beb7f4
Jake Buob <jake@mojotech.com>
2013-11-08 20:03:23 -0500

    Get rid of garbage in Pulse Scaffold
    Also scope pulse to current_user

--- app/controllers/pulses_controller.rb
+++ app/controllers/pulses_controller.rb
@@ -1,46 +1,15 @@
 class PulsesController < ApplicationController
   skip_before_filter :authenticate_user!, only: :show
-  
-  before_action :set_pulse, only: [:show, :edit, :update, :destroy]
 
-  # GET /pulses
-  # GET /pulses.json
-  def index
-    @pulses = Pulse.all
-  end
+  before_action :set_pulse, only: [:edit, :update]
 
-  # GET /pulses/1
-  # GET /pulses/1.json
   def show
+    @pulse = Pulse.find(params[:id])
   end
 
-  # GET /pulses/new
-  def new
-    @pulse = Pulse.new
-  end
-
-  # GET /pulses/1/edit
   def edit
   end
 
-  # POST /pulses
-  # POST /pulses.json
-  def create
-    @pulse = Pulse.new(pulse_params)
-
-    respond_to do |format|
-      if @pulse.save
-        format.html { redirect_to @pulse, notice: 'Pulse was successfully created.' }
-        format.json { render action: 'show', status: :created, location: @pulse }
-      else
-        format.html { render action: 'new' }
-        format.json { render json: @pulse.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /pulses/1
-  # PATCH/PUT /pulses/1.json
   def update
     respond_to do |format|
       if @pulse.update(pulse_params)
@@ -53,24 +22,12 @@ class PulsesController < ApplicationController
     end
   end
 
-  # DELETE /pulses/1
-  # DELETE /pulses/1.json
-  def destroy
-    @pulse.destroy
-    respond_to do |format|
-      format.html { redirect_to pulses_url }
-      format.json { head :no_content }
-    end
-  end
-
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_pulse
-      @pulse = Pulse.find(params[:id])
+      @pulse = current_user.pulse
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def pulse_params
-      params.require(:pulse).permit(:user_id, :url)
+      params.require(:pulse).permit(:url)
     end
 end
