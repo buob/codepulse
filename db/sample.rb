18f2a883abf6517bfc70d64acd5302286b11f8ec
Jake Buob <jake@mojotech.com>
2013-11-17 17:23:32 -0500

    Change tagline to job title

--- db/sample.rb
+++ db/sample.rb
@@ -3,7 +3,7 @@ user = User.find_or_create_by!(email: 'jake@mojotech.com') do |user|
   user.password_confirmation = 'foobar123'
 end
 
-user.pulse.update_attributes(tagline: 'engineer --with class')
+user.pulse.update_attributes(tagline: 'engineer at MojoTech')
 
 SocialProfile.all.each do |profile|
   user.social_accounts << SocialAccount.create(social_profile: profile, handle: 'jakebuob')
