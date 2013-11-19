5148701db30c3cf0bb3d60052eb2e22ade8fd495
Jake Buob <jake@mojotech.com>
2013-11-19 02:54:26 -0500

    Add LinkedIn to Seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,6 +3,7 @@ social_profiles = [
   {name: 'facebook', url: 'http://facebook.com/', color: '#43609c', display_order: 2},
   {name: 'dribbble', url: 'http://dribbble.com/', color: '#cb376e', display_order: 3},
   {name: 'twitter', url: 'http://twitter.com/', color: '#00aeed', display_order: 4},
+  {name: 'linkedin', url: 'http://linkedin.com/in/', color: '#23639b', display_order: 5},
 ]
 
 social_profiles.each do |p|
